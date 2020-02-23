f7536ff082aa07a3f276b97a9a4a1bf92a8b3c8c
Junwei LI <jlyc8@bu.edu>
2020-02-22 19:15:21 -0500

    creat main and process

--- ImageToVideo.py
+++ ImageToVideo.py
@@ -1,10 +1,12 @@
-import os
 import subprocess
 
 def imgToVideo(username):
-  fileName =  'processed_imgs/'+ username +'img' +'%d'+'.png'
-  normalVideo =  "video/"  + username + "normal.avi"
-  betterVideo =  "video/"  + username + "better.mp4"
+  fileName =  'processed_imgs/'+
+                       username +
+                      'img' +'%d'+'.png'
+
+  normalVideo =  "video/" + username + "normal.avi"
+  betterVideo =  "video/" + username + "better.mp4"
 
   # convert jpg to mp4
   # ffmpeg -i img-%02d.png video_name.avi #2-digit number for name
@@ -15,7 +17,6 @@ def imgToVideo(username):
     fileName, 
     normalVideo])
 
-  # convert to better quality
   subprocess.call(['ffmpeg', '-i', normalVideo, '-c:a', 'copy', 
     '-c:v', 'copy', '-r', '30', '-s', 'hd720', '-b:v', '2M', 
     betterVideo])
--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import twitter_handling as tw
 import image_processing as imagp
+import ImageToVideo as ITV
+import threading
+import time
+import queue
 
-twitter = tw.twitter_feed_catching("keys")
 
-# consider wrong user id input 
-username = input("Twitter ID: ")
-profile_url = twitter.get_user_pic(username)
-tweets = twitter.get_users_tweets(username)
+def grab_tweets_process(q,completion_queue):
+    username = q.get()
+    profile_url = twitter.get_user_pic(username)
+    tweets = twitter.get_users_tweets(username)
+    imagp.create_images(username, profile_url, tweets)
+    completion_queue.put(username)
 
-# creat image and store into folder
-imagp.create_images(username, profile_url, tweets)
+
+def video_process(completion_queue):
+    username =  completion_queue.get()
+    ITV.imgToVideo(username) 
+
+if __name__ == '__main__':
+  q1 = queue.Queue(maxsize=20)
+  completion_q1 = queue.Queue()
+  twitter = tw.twitter_feed_catching("keys")
+
+  while True:
+    if not q1.full():
+      id = input("Twitter id?")
+      q1.put(id)
+    else:
+      print("wait a minute...")
+    if not q1.empty():
+      t1 = threading.Thread(name="grab_tweets_process", target=grab_tweets_process, args=(q1, completion_q1))
+      t1.start()    
+    if not completion_q1.empty():
+      t2 = threading.Thread(name="video_process", target=video_process, args=(completion_q1))
+      t2.start()
